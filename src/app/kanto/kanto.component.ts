c9c11c82961b298f56e87d9baf8295800704bbae
Jason <jason.w.switzer@gmail.com>
2017-11-26 21:32:26 -0800

    added jquery functionality

--- src/app/kanto/kanto.component.ts
+++ src/app/kanto/kanto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiserviceService } from './../apiservice.service';
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-kanto',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kanto.component.css']
 })
 export class KantoComponent implements OnInit {
+  pokemon;
+  html_string;
+  dexId;
+  dex_entry;
+  mon_name;
+  height;
+  weight;
+  types;
+  abilities;
+  stats;
 
-  constructor() { }
+
+  
+
+  constructor(private _apiserviceService: ApiserviceService) { }
 
   ngOnInit() {
+  	this.pokemon = this._apiserviceService.getPokemon('gen1')
+  	.then( pokemon => {
+  		console.log(pokemon)
+  	this.html_string = "";
+      for(var i = 1; i <= 151; i++) {
+        this.html_string += '<img id="' +  [i]  + '" src="http://pokeapi.co/media/img/' + [i] + '.png" alt="">';      
+      }
+    $("#container").html(this.html_string);  
+
+  	});
   }
 
 }
